4e9dbaca8df9844274918a70df5e54ccc0c1dcc3
2019211001001026-LiuDingKun <1303552704@qq.com>
2021-03-16 18:45:22 +0800

    HW-Week2 Commit

---
+++ src/main/java/com/dabing/week2/demo/HelloWorldServlet.java
@@ -0,0 +1,25 @@
+
+package com.dabing.week2.demo;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+//now its just
+//extend HttpServlet
+public class HelloWorldServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        //when client request method is GET-here-inside doGet()
+        //we want to send Hello to client
+        //we need to write Hello in response
+        //get writer-java.io
+        PrintWriter writer=response.getWriter();
+        writer.println("Hello Client!!!");//that all
+    }
+    public void doPost(HttpServletRequest request, HttpServletResponse response){
+//when client request method is GET-here-inside doPost()
+    }
+}
---
+++ src/main/webapp/Myjsp.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 刘定坤
+  Date: 2021/3/16
+  Time: 11:12
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<a href="index.jsp"> go to Ecjtu </a><!-- method is GET -->
+<form method="post"> <!--what is method when we use form?--><!--its GET why? default is GET, form data is added in the URL ,u can see-->
+<!--its better to usePOST in form, data is not added in the URL-->
+    Name:<input type="text" name="name"><br/>
+    ID:<input type="text" id="id"><br/>
+    <input type="submit" value="Send data to server"/>
+</form>
+</body>
+</html>
